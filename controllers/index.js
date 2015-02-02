8c9dcace6c31260ab9a394457eb3ea5145a86fa9
coolicer <coolicer@foxmail.com>
2015-02-02 16:00:36 +0800

    'xxx'

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,16 @@
-var ejs = require('../lib/ejs')
+var h = require('../lib/h')
 
-function index(r){
+function index(r) {
     var res = r.response
     var data = {
-        title: '首页',
-        text: '你好,fibjs'
-    } 
-    res.write( ejs.render('index.ejs', data) )
+        title: 'Layout Test',
+        items: [
+            'apple',
+            'orange',
+            'banana'
+        ]
+    };
+    res.write(h.render('test.html', data))
 }
 
 module.exports = index;
